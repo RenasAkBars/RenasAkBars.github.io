348d13f807380a357a45f0ae2ffd1d01cfe69971
RenasAkBars <bezbuldyrabyz@gmail.com>
2018-04-30 20:05:13 +0300

    30 Apr 2018

--- Projects/PlumProExteriors/scripts.js
+++ Projects/PlumProExteriors/scripts.js
@@ -1,5 +1,7 @@
 
+//---begin nav menu hover code
 $('.dropdown-toggle').click(function(e) {
+    alert($(document).width());
     if ($(document).width() > 992) {
 /*        var url = $(this).attr('href');
         if (url !== '#') {
@@ -8,6 +10,30 @@ $('.dropdown-toggle').click(function(e) {
         return false;
     }
 });
+//---end nav menu hover code
 
+
+
+//---begin our services add plums code
 $('.sub-menu .nav-color, footer .services-link').prepend('<span class="plumicon plum-plum"></span> ');
+//---end our services add plums code
 
+
+
+//---begin carousel set height code
+var $carItem = $('.carousel-item');
+
+setHeight($carItem, 90);// устанавливаем высоту окна при первой загрузке страницы
+$(window).resize( setCarItem ); // обновляем при изменении размеров окна
+
+function setCarItem() {
+    setHeight($carItem, 90);
+}
+
+function setHeight($item, percentage) {
+    var h = $(window).height() * percentage / 100 + 'px';
+    $item.css({
+        height: h
+    });
+}
+//---begin carousel set height code
